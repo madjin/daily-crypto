b7b49e6c952e7e9ffab1181d40477b103b7f2138
jin <32600939+madjin@users.noreply.github.com>
2023-03-19 20:36:46 -0400

    add clean up step

--- daily_blender.sh
+++ daily_blender.sh
@@ -4,3 +4,5 @@
 wget https://anata.dev/blender.tar.xz
 tar xvf blender.tar.xz --strip 1 -C blender
 ./blender/blender -b -P create_cubes.py
+echo "Cleaning up files"
+rm -rf blender.tar.xz ./blender
