387c0beda5fc22698431d90ad15231f1e36aac7e
jin <32600939+madjin@users.noreply.github.com>
2023-03-19 21:43:34 -0400

    clean up folders

--- daily_blender.sh
+++ daily_blender.sh
@@ -6,4 +6,4 @@ tar xvf blender.tar.xz --strip 1 -C blender
 ./blender/blender -b -P create_cubes.py
 echo "Cleaning up files"
 rm blender.tar.xz
-find ./blender ! -name 'README.md' -type f -delete
+find ./blender -type f ! -name "README.md" -delete && find ./blender -mindepth 1 -type d ! -name "README.md" -exec rm -r {} +
