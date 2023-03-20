746fe16d73457983798196309602ee9b074e307c
jin <32600939+madjin@users.noreply.github.com>
2023-03-19 20:31:11 -0400

    update daily_blender.sh

--- daily_blender.sh
+++ daily_blender.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Make sure dependencies are installed
-packages=("blender")
 
-for package in "${packages[@]}"; do
-    if ! dpkg-query -W --showformat='${Status}\n' "$package" | grep -q "install ok installed"; then
-        echo "Package '$package' is not installed, installing now..."
-        sudo apt-get update && sudo apt-get install -y "$package"
-    else
-        echo "Package '$package' is already installed."
-    fi
-done
-
-blender -b -P create_cubes.py
+wget https://anata.dev/blender.tar.xz
+tar xvf blender.tar.xz --strip 1 -C blender
+./blender/blender -b -P create_cubes.py
