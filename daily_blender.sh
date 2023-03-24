4d874bd7026de7f34536af733b63e1fb1e3bcfc3
jin <32600939+madjin@users.noreply.github.com>
2023-03-23 22:01:57 -0400

    Update daily_blender.sh

--- daily_blender.sh
+++ daily_blender.sh
@@ -6,4 +6,4 @@ tar xvf blender.tar.xz --strip 1 -C blender
 ./blender/blender -b -P create_cubes.py
 echo "Cleaning up files"
 rm blender.tar.xz
-find ./blender -type f ! -name "README.md" -delete && find ./blender -mindepth 1 -type d ! -name "README.md" -exec rm -r {} +
+rm -rv !README.md ./blender/*
