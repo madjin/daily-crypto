3a69118faa2eccb76c2aa44287f2a1c928013f87
jin <32600939+madjin@users.noreply.github.com>
2023-03-25 04:13:37 -0400

    update blender part

--- daily_blender.sh
+++ daily_blender.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+mkdir blender
 wget https://anata.dev/blender.tar.xz
 tar xvf blender.tar.xz --strip 1 -C blender
 ./blender/blender -b -P create_cubes.py
 echo "Cleaning up files"
 rm blender.tar.xz
-rm -rv !README.md ./blender/*
+rm -rf ./blender
